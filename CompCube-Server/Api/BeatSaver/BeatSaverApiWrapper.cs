f893b8a65ab35ea5ba53e08ee5575485864dc7c7
Bela Varga <100267928+NotBela@users.noreply.github.com>
2025-11-09 15:45:03 -0500

    misspelled compcube-server 😭

--- CompCube-Server/Api/BeatSaver/BeatSaverApiWrapper.cs
+++ CompCube-Server/Api/BeatSaver/BeatSaverApiWrapper.cs
@@ -5,7 +5,7 @@ namespace CompCube_Server.Api.BeatSaver;
 
 public class BeatSaverApiWrapper
 {
-    private readonly BeatSaverSharp.BeatSaver _beatSaver = new(new BeatSaverOptions("CompCube_Server", new Version("1.0.0")));
+    private readonly BeatSaverSharp.BeatSaver _beatSaver = new(new BeatSaverOptions("CompCube-Server", new Version("1.0.0")));
 
     public async Task<Beatmap?> GetBeatmapFromHash(string hash) => await _beatSaver.BeatmapByHash(hash);
 
