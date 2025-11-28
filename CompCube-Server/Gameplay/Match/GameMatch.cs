12f125efad3af0b1775b9ded22c3a2d6930a6d55
Bela Varga <100267928+NotBela@users.noreply.github.com>
2025-11-28 15:12:46 -0500

    do more with matches

--- CompCube-Server/Gameplay/Match/GameMatch.cs
+++ CompCube-Server/Gameplay/Match/GameMatch.cs
@@ -1,4 +1,5 @@
-﻿using CompCube_Server.Interfaces;
+﻿using CompCube_Models.Models.Match;
+using CompCube_Server.Interfaces;
 
 namespace CompCube_Server.Gameplay.Match;
 
@@ -9,6 +10,8 @@ public class GameMatch
     private readonly Dictionary<IConnectedClient, Team> _teams = new();
 
     private readonly Dictionary<Team, int> _points = new();
+
+    private ScoreManager? _currentRoundScoreManager = null;
     
     public void Init(IConnectedClient[] red, IConnectedClient[] blue, MatchSettings settings)
     {
@@ -16,6 +19,9 @@ public class GameMatch
         
         SetupPlayer(red, Team.Red);
         SetupPlayer(blue, Team.Blue);
+        
+        _points.Add(Team.Red, 0);
+        _points.Add(Team.Blue, 0);
         
         return;
         
@@ -29,6 +35,28 @@ public class GameMatch
             }
         }
     }
+
+    public async Task StartMatchAsync()
+    {
+        _currentRoundScoreManager = new ScoreManager(_teams);
+        
+        _currentRoundScoreManager.OnScoresSubmitted += HandleScores;
+    }
+
+    private void HandleScores(Dictionary<IConnectedClient, Score> scores)
+    {
+        var redPoints = scores.Where(i => _teams[i.Key] == Team.Red).Sum(i => i.Value.Points);
+        var bluePoints = scores.Where(i => _teams[i.Key] == Team.Blue).Sum(i => i.Value.Points);
+
+        if (redPoints >= bluePoints)
+            _points[Team.Red] += 1;
+
+        if (bluePoints >= redPoints)
+            _points[Team.Blue] += 1;
+        
+    }
+
+    public void StartMatch() => StartMatchAsync();
 
     private void HandleClientDisconnect(IConnectedClient client)
     {
--- CompCube-Server/Gameplay/Match/ScoreManager.cs
+++ CompCube-Server/Gameplay/Match/ScoreManager.cs
@@ -1,11 +1,46 @@
-﻿using CompCube_Server.Interfaces;
+﻿using CompCube_Models.Models.Match;
+using CompCube_Models.Models.Packets.UserPackets;
+using CompCube_Server.Interfaces;
 
 namespace CompCube_Server.Gameplay.Match;
 
 public class ScoreManager
 {
-    public ScoreManager(IConnectedClient clients)
+    private readonly Dictionary<IConnectedClient, Score?> _scores;
+
+    public event Action<Dictionary<IConnectedClient, Score>>? OnScoresSubmitted;
+    
+    public ScoreManager(Dictionary<IConnectedClient, GameMatch.Team> players)
     {
+        _scores = players.Select(i => new KeyValuePair<IConnectedClient, Score?>(i.Key, null)).ToDictionary();
+
+        foreach (var player in players)
+        {
+            player.Key.OnScoreSubmission += HandleScoreSubmitted;
+        }
+    }
+
+    private void HandleScoreSubmitted(ScoreSubmissionPacket score, IConnectedClient client)
+    {
+        client.OnScoreSubmission -= HandleScoreSubmitted;
         
+        _scores[client] = score.GetScore();
+        
+        CheckIfAllScoresAreSubmitted();
+    }
+
+    private void CheckIfAllScoresAreSubmitted()
+    {
+        if (_scores.Any(i => i.Value == null))
+            return;
+        
+        OnScoresSubmitted?.Invoke(_scores.Select(i => new KeyValuePair<IConnectedClient,Score>(i.Key, i.Value ?? Score.Empty)).ToDictionary());
+    }
+
+    public void HandlePlayerDisconneced(IConnectedClient player)
+    {
+        player.OnScoreSubmission -= HandleScoreSubmitted;
+        
+        CheckIfAllScoresAreSubmitted();
     }
 }
