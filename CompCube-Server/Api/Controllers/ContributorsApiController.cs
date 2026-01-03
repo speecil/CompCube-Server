557d7b0a7a3aff8898974c2c5947b29525198a63
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-01-03 01:47:49 -0500

    added contributors

---
+++ CompCube-Server/Api/Controllers/ContributorsApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompCube_Server.Api.Controllers;
+
+[ApiController]
+public class ContributorsApiController : ControllerBase
+{
+    [HttpGet("api/contributors")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<>
+}
