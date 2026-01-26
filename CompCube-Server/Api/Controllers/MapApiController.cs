3bbdee05709e0fd65ea10f795f0ea71a599b0009
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-01-25 22:06:22 -0500

    add playlist api endpoint

--- CompCube-Server/Api/Controllers/MapApiController.cs
+++ CompCube-Server/Api/Controllers/MapApiController.cs
@@ -1,5 +1,7 @@
-﻿using CompCube_Server.SQL;
+﻿using CompCube_Models.Models.Map;
+using CompCube_Server.SQL;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace CompCube_Server.Api.Controllers;
 
@@ -9,4 +11,63 @@ public class MapApiController(MapData mapData) : ControllerBase
     [HttpGet("/api/maps/hashes")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<string[]> GetAllMapHashes() => mapData.GetAllMaps().Select(i => i.Hash).ToArray();
+
+    [HttpGet("/api/maps/playlist")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<string> GetPlaylist()
+    {
+        var allMaps = mapData.GetAllMaps();
+
+        var songs = new List<PlaylistSong>();
+
+        foreach (var song in allMaps)
+        {
+            if (songs.Any(i => i.Hash == song.Hash))
+                continue;
+            
+            var allSimilarHashes = allMaps.Where(i => i.Hash.ToLower() == song.Hash.ToLower());
+
+            var playlistSong = new PlaylistSong(song.Hash, allSimilarHashes.Select(i => i.Difficulty).ToArray());
+            
+            songs.Add(playlistSong);
+        }
+
+        var jObject = new JObject
+        {
+            {"playlistTitle", "CompCube Maps"},
+            {"playlistAuthor", "CompCube Team"},
+            {"songs", new JArray(songs.Select(i => i.GetJsonObject()))}
+        };
+
+        return jObject.ToString();
+    }
+}
+
+public class PlaylistSong(string hash, VotingMap.DifficultyType[] difficultyTypes)
+{
+    public readonly string Hash = hash;
+
+    public readonly VotingMap.DifficultyType[] DifficultyTypes = difficultyTypes;
+
+    public JObject GetJsonObject()
+    {
+        var difficultiesObject = new JArray();
+
+        foreach (var diff in DifficultyTypes)
+        {
+            difficultiesObject.Add(new JObject
+            {
+                {"characteristic", "Standard"},
+                {"name", diff.ToString()}
+            });
+        }
+        
+        var jObject = new JObject
+        {
+            {"hash", Hash},
+            {"difficulties", difficultiesObject}
+        };
+
+        return jObject;
+    }
 }
